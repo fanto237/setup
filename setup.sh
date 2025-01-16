06e4e096ac00427686451af017586e437813f0de
Lucien <me@fanto.dev>
2025-01-16 21:11:26 +0100

    improve setup script

--- setup.sh
+++ setup.sh
@@ -1,92 +1,208 @@
 #!/bin/bash
 
-# *********************************************************
-# *********************************************************
-# **                                                     **
-# **               SET UP UBUNTU WSL                     **
-# **                                                     **
-# *********************************************************
-# *********************************************************
-
-HOST_NAME=$(wslpath "$(wslvar USERPROFILE)" | cut -d '/' -f 5)
-GIT_DIR_PATH="/mnt/c/Users/${HOST_NAME}/git"
-LSD_BINARY="lsd_0.23.1_amd64.deb"
-
 RED='\033[0;31m'  # Red Color
 BLUE='\033[0;34m' # Blue Color
 NC='\033[0m'      # No Color
 
-# *** Install latest ubuntu packages ***
-echo -e "${BLUE}Updating ubuntu packages${NC}"
-sudo apt update && sudo apt upgrade -y
-echo -e "${RED}Ubuntu has been updated${NC}"
-sleep 5
+echo -e "${BLUE}  ************************************************************${NC}"
+echo -e "${BLUE}  ************************************************************${NC}"
+echo -e "${BLUE}  **                                                        **${NC}"
+echo -e "${BLUE}  **            BASH SCRIPT TO SET UP ENV                   **${NC}"
+echo -e "${BLUE}  **                                                        **${NC}"
+echo -e "${BLUE}  ************************************************************${NC}"
+echo -e "${BLUE}  ************************************************************${NC}"
 
-# *** Enhancing the cli with some charms ***
-# (1) installing lsd
-echo -e "${BLUE}Installing LS Super charged${NC}"
+# Function to set up Ubuntu WSL
+setup_wsl() {
+  setup_linux "wsl"
+}
 
-sudo dpkg -i $LSD_BINARY
-rm -f $LSD_OUTPUT
-echo -e "${RED}LSD has been installed ${NC}"
-sleep 5
+# Function to set up Windows Terminal (Placeholder)
+setup_windows() {
 
-# (2) setting up zsh
-echo -e "${BLUE}Setting up zsh and its plugings${NC}"
+  echo "Setting up Windows"
+  read -p "Do you want to install scoop? (y/n): "
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    install_scoop
+  fi
+  sleep 5
 
-# echo -e "${RED}creating directory to store plugins${NC}"
-# mkdir -p ~/.zsh/.plugins
+  # *** Install zsh
+  setup_zsh
+  sleep 5
 
-# echo -e "${RED}cd to the plugins directory${NC}"
-# cd ~/.zsh/.plugins
+  # restore scoop packages
+  echo -e "${BLUE}Restoring scoop packages${NC}"
+  scoop import scoop.json
+  sleep 3
 
-echo -e "${RED}cloning p10k${NC}"
-git clone https://github.com/romkatv/powerlevel10k.git .zsh/.plugins/powerlevel10k
+  # ask if fancontroll config should be copy
+  read -p "Do you want to copy the fancontrol config? (y/n): "
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    cp fancontroll.sjon ~/scoop/apps/fancontroll/current/Configurations
+  fi
+}
 
-echo -e "${RED}cloning zsh-completion${NC}"
-git clone https://github.com/zsh-users/zsh-completions.git .zsh/.plugins/zsh-completions
+# Function to set up Linux
+setup_linux() {
+  echo "Setting up Linux"
 
-echo -e "${RED}cloning zsh-syntax-highlighting${NC}"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .zsh/.plugins/zsh-syntax-highlighting
+  # *** Install latest ubuntu packages ***
+  echo -e "${BLUE}Updating ubuntu packages${NC}"
+  sudo apt update && sudo apt upgrade -y
+  sleep 5
 
-echo -e "${RED}cloning zsh-autosuggestions${NC}"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git .zsh/.plugins/zsh-autosuggestions
+  # *** Enhancing the cli with some charms ***
+  # installing lsd
+  echo -e "${BLUE}Installing LS Super charged${NC}"
+  sudo apt install lsd -y
+  sleep 5
 
-echo -e "${RED}All Plugins have been cloned and added to zsh plugins directory${NC}"
+  # *** Install zsh
+  setup_zsh $1
+  sleep 5
 
-# *** Install zsh
-echo -e "${BLUE}Installing zsh${NC}"
-sudo apt install zsh -y
-sleep 5
+  # *** setting up my git working dir ***
+  setup_working_dir $1
+  sleep 5
 
-# *** setting up my git working dir ***
-echo -e "${BLUE}Creating the git working dir${NC}"
+  # *** installing dev dependencies
+  # install node
+  install_node_js
+  sleep 5
 
-# (1) Creating the git dir
-mkdir -p $GIT_DIR_PATH
+  # install docker
+  if [[ $1 == "wsl" ]]; then
+    echo -e "${BLUE}Docker is not supported on WSL${NC}"
+  else
+    install_docker
+  fi
+  sleep 5
 
-# (2) Making a symbolic link of the git directoy into the wsl
-ln -s $GIT_DIR_PATH
-echo -e "${RED}The git working dir has been set up successfully${NC}"
-sleep 5
+  # Setting zsh as the default shell
+  echo -e "${BLUE}Setting zsh as the default shell${NC}"
+  chsh -s "$(which zsh)"
+}
 
-# *** installing dev dependencies
-echo -e "${BLUE}Installing dev dependencies${NC}"
+install_scoop() {
+  # Install scoop
+  echo -e "${BLUE}Installing scoop${NC}"
+  Powershell
+  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+  Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+  sleep 5
+  exit
+}
 
-# (1) node and yarn
-echo -e "${RED} Installing node and yarn${NC}"
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install nodejs -y
-npm install --global yarn
-echo -e "${RED}Node and Yarn has been installed${NC}"
-sleep 5
+setup_zsh() {
+  echo -e "${BLUE}Setting up zsh and its plugings${NC}"
 
-# setting zsh as the default shell
-echo -e "${BLUE}Setting zsh as the default shell${NC}"
-chsh -s $(which zsh)
+  echo -e "${RED}cloning p10k${NC}"
+  git clone https://github.com/romkatv/powerlevel10k.git .zsh/.plugins/powerlevel10k
+
+  echo -e "${RED}cloning zsh-completion${NC}"
+  git clone https://github.com/zsh-users/zsh-completions.git .zsh/.plugins/zsh-completions
+
+  echo -e "${RED}cloning zsh-syntax-highlighting${NC}"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .zsh/.plugins/zsh-syntax-highlighting
+
+  echo -e "${RED}cloning zsh-autosuggestions${NC}"
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git .zsh/.plugins/zsh-autosuggestions
+
+  echo -e "${RED}All Plugins have been cloned and added to zsh plugins directory${NC}"
+
+  mv .zshrc ~
+  mv .p10k.zsh ~
+
+  echo -e "${RED}The zshrc file has been moved to the home directory${NC}"
+
+  if [[ $1 == "wsl" ]]; then
+    mv .wezterm.lua ~
+  else
+    echo -e "${BLUE}Installing zsh${NC}"
+    sudo apt install zsh -y
+    mv clean-dl.sh ~
+  fi
+}
+
+install_node_js() {
+  echo -e "${BLUE}Installing Node.js and Yarn${NC}"
+  sudo apt install curl -y
+  # Download and install fnm:
+  curl -o- https://fnm.vercel.app/install | bash
+
+  # Download and install Node.js:
+  fnm install 22
+
+  # Verify the Node.js version:
+  node -v # Should print "v22.13.0".
+
+  # Download and install Yarn:
+  corepack enable yarn
+
+  # Verify Yarn version:
+  yarn -v
+}
+
+install_docker() {
+  echo -e "${BLUE}Installing Docker${NC}"
+  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+  # Add Docker's official GPG key:
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt-get update
+
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  sudo groupadd docker
+
+  sudo usermod -aG docker $USER
+}
+
+setup_working_dir() {
+  echo -e "${BLUE}Creating the git working dir${NC}"
+
+  if [[ $1 == "wsl" ]]; then
+    HOST_NAME=$(cmd.exe /c "echo %USERNAME%")
+    GIT_DIR_PATH="/mnt/c/Users/${HOST_NAME}/repos"
+
+    ln -s $GIT_DIR_PATH
+    echo -e "${RED}The git working dir has been set up successfully${NC}"
+    sleep 5
+  else
+    mkdir repos
+  fi
+}
+
+# Main script execution
+echo "Select setup option:"
+echo "1. Ubuntu"
+echo "2. Windows Terminal"
+echo "3. Windows Subsystem for Linux"
+read -p "Enter your choice (1-3): " choice
+
+case "$choice" in
+1)
+  setup_wsl
+  ;;
+2)
+  setup_windows
+
+  ;;
+3)
+  setup_linux
+  ;;
+*)
+  echo "Invalid choice, please run the script again."
+  ;;
+esac
