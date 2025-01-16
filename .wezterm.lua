4c0faa39c30c9a019441928a98e8eca179da487b
Lucien <me@fanto.dev>
2025-01-16 21:10:29 +0100

    add improved config files

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,12 +15,9 @@ end
 
 config.hide_tab_bar_if_only_one_tab = true
 config.force_reverse_video_cursor = true
-config.font_size =18
+config.font_size = 13
 
-config.font_dirs = {'./.wezterm/ComicShannsMono', 'fonts'}
--- config.font = wezterm.font( 'ComicsShann Mono' )
--- config.font = wezterm.font( 'Iosevka Term' )
-config.font = wezterm.font( 'JetBrains Mono' )
+config.font = wezterm.font( 'ComicShannsMono Nerd Font' )
 --config.font = wezterm.font( 'Andale Mono' )
 
 config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
@@ -45,8 +42,14 @@ if string.find(wezterm.target_triple, 'apple') ~= nil then
   config.send_composed_key_when_left_alt_is_pressed = true
 end
 
-
 config.keys = {
+  -- Turn off non-breaking spaces in wezterm on MacOS
+  {
+    key = ' ',
+    mods = 'OPT',
+    action = wezterm.action.SendKey { key = ' '},
+  },
+
   {
     key = 'w',
     mods = 'SHIFT|CTRL',
@@ -54,90 +57,12 @@ config.keys = {
   },
 }
 
--- config.color_scheme = 'Builtin Dark'
+config.color_scheme = 'Builtin Dark'
 -- config.color_scheme = 'Builtin Light'
 -- config.color_scheme = 'Builtin Solarized Dark'
 -- config.color_scheme = 'Builtin Solarized Light'
--- config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Night Owl (Gogh)'
+config.color_scheme = "OneDark (base16)"
 
--- -----------------------------
--- config.colors = {
---   -- The default text color
---   foreground = 'silver',
---   -- The default background color
---   background = 'black',
-
---   -- Overrides the cell background color when the current cell is occupied by the
---   -- cursor and the cursor style is set to Block
---   cursor_bg = '#52ad70',
---   -- Overrides the text color when the current cell is occupied by the cursor
---   cursor_fg = 'black',
---   -- Specifies the border color of the cursor when the cursor style is set to Block,
---   -- or the color of the vertical or horizontal bar when the cursor style is set to
---   -- Bar or Underline.
---   cursor_border = '#52ad70',
-
---   -- the foreground color of selected text
---   selection_fg = 'black',
---   -- the background color of selected text
---   selection_bg = '#fffacd',
-
---   -- The color of the scrollbar "thumb"; the portion that represents the current viewport
---   scrollbar_thumb = '#222222',
-
---   -- The color of the split lines between panes
---   split = '#444444',
-
---   ansi = {
---     'black',
---     'maroon',
---     'green',
---     'olive',
---     'navy',
---     'purple',
---     'teal',
---     'silver',
---   },
---   brights = {
---     'grey',
---     'red',
---     'lime',
---     'yellow',
---     'blue',
---     'fuchsia',
---     'aqua',
---     'white',
---   },
-
---   -- Arbitrary colors of the palette in the range from 16 to 255
---   indexed = { [136] = '#af8700' },
-
---   -- Since: 20220319-142410-0fcdea07
---   -- When the IME, a dead key or a leader key are being processed and are effectively
---   -- holding input pending the result of input composition, change the cursor
---   -- to this color to give a visual cue about the compose state.
---   compose_cursor = 'orange',
-
---   -- Colors for copy_mode and quick_select
---   -- available since: 20220807-113146-c2fee766
---   -- In copy_mode, the color of the active text is:
---   -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
---   -- 2. selection_* otherwise
---   copy_mode_active_highlight_bg = { Color = '#000000' },
---   -- use `AnsiColor` to specify one of the ansi color palette values
---   -- (index 0-15) using one of the names "Black", "Maroon", "Green",
---   --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
---   -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
---   copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
---   copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
---   copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
-
---   quick_select_label_bg = { Color = 'peru' },
---   quick_select_label_fg = { Color = '#ffffff' },
---   quick_select_match_bg = { AnsiColor = 'Navy' },
---   quick_select_match_fg = { Color = '#ffffff' },
--- }
--- -----------------------------
 -- and finally, return the configuration to wezterm
 return config
+
--- .zshrc
+++ .zshrc
@@ -5,7 +5,7 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-HOST_USERNAME=$(wslpath "$(wslvar USERPROFILE)" | cut -d '/' -f 5)
+HOST_USERNAME=$(cmd.exe /c "echo %USERNAME%")
 
 
 # The following lines were added by compinstall
